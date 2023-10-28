df7c2e319251d7ac3d8352d423b5c042f9ccbbdb
Dean Tahory <dean.tahory@gmail.com>
2023-10-29 01:21:58 +0300

    add logo and styled navbar

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -8,13 +8,13 @@ import { Moon, Sun } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 
-export function ModeToggle() {
+const ModeToggle: React.FC<React.ComponentProps<"div">> = ({ className }) => {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" className={className}>
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
@@ -27,4 +27,6 @@ export function ModeToggle() {
       </DropdownMenuContent>
     </DropdownMenu>
   );
-}
+};
+
+export default ModeToggle;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
-import { ModeToggle } from "./mode-toggle";
+import ModeToggle from "./mode-toggle";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
   return (
-    <div className="container py-4 px-2 sm:px-4 flex justify-end">
-      <ModeToggle />
+    <div className="container relative py-4 px-2 sm:px-4 flex justify-center">
+      <p className="mx-auto text-lg">Hasbara AI</p>
+      <ModeToggle className="absolute right-2 lg:right-0 top-1/2 transform -translate-y-1/2" />
     </div>
   );
 };
